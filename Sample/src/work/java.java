579713cbbaba700bf1db81b1c60b778afff0dca6
sator <sator@10.29.86.204>
2025-12-02 12:11:54 +0900

    JAVA導入の提出

--- Sample/src/work/java.java
+++ Sample/src/work/java.java
@@ -4,7 +4,16 @@ public class java {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+		
+		String name = "山田花子";
+		int age = 25;
+		String hobby = "映画鑑賞";
+		
+		System.out.println("私の名前は" + name +"です。");
+		System.out.println("年齢は"+ age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
 	}
 
 }
